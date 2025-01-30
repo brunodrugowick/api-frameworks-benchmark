ef87d7cf0217d06b86fa5e2bb7e63a5bd64aef3b
Bruno Drugowick <brunodrugowick@users.noreply.github.com>
2025-01-29 23:51:49 -0300

    Fixes web server ports to read the PORT env var.

--- go-http-server/main.go
+++ go-http-server/main.go
@@ -3,18 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/brunodrugowick/go-http-server-things/pkg/server"
 )
 
 func main() {
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		portString = "8080"
+	}
+
+	port, err := strconv.Atoi(portString)
+	if err != nil {
+		log.Fatalf("Invalid port: %v", err)
+	}
+
 	apiPathHandler := server.NewDefaultPathHandlerBuilder("/api").
 		WithHandlerFunc("/", func(w http.ResponseWriter, r *http.Request) {
 			w.Write([]byte("Hello, Golang!"))
 		}).Build()
 
 	srv := server.NewDefaultServerBuilder().
-		SetPort(8085).
+		SetPort(port).
 		WithPathHandler(apiPathHandler).Build()
 
 	log.Fatal(srv.ListenAndServe())
